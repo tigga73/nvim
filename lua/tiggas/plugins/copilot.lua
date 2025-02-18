84df41f9edb0e2393618410006c62c7546c2e7d3
Tiago Cardoso <tiagocardoso@velocityglobal.com>
2025-02-18 16:53:50 -0300

    feat: adding snipe plugin and small refactors

--- lua/tiggas/plugins/copilot.lua
+++ lua/tiggas/plugins/copilot.lua
@@ -3,6 +3,6 @@ return {
   config = function()
     -- Configurações específicas do Copilot (se necessário)
     vim.g.copilot_no_tab_map = true -- Opcional: impede que o Copilot use a tecla Tab
-    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+    vim.api.nvim_set_keymap("i", "<C-a>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
   end,
 }
